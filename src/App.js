902ba9eea258dcb9fbcf5face6b2492e7eacf15e
hamzabelakri <hamzabelakri92@gmail.com>
2022-10-28 21:09:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useEffect, useState} from "react";
+
 import './App.css';
 
 function App() {
+
+
+  const hours = new Date().getHours();
+  const minutes = new Date().getMinutes();
+  const seconds = new Date().getSeconds();
+  const ampm = hours>12? "PM" : "AM";
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h2>Digital Clock</h2>
+    <div className="clock">
+      <div>
+        <span id="hour">{ hours < 10 ? "0" + hours : hours}</span>
+        <span className="text">Hours</span>
+      </div>
+      <div>
+        <span id="minutes">{minutes < 10 ? "0" + minutes : minutes}</span>
+        <span className="text">Minutes</span>
+      </div>
+      <div>
+        <span id="seconds">{seconds < 10 ? "0" + seconds : seconds}</span>
+        <span className="text">Seconds</span>
+      </div>
+      <div>
+        <span id="ampm">{ampm}</span>
+      </div>
+    </div>
     </div>
   );
 }
